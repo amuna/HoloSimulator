c8bfdec49fc20c78929d7eab4581371738bbafb5
ahmednaeem <ahmed.naeem@uoit.net>
2018-11-20 01:56:41 -0500

    Implemented Home

--- app/src/main/java/com/example/ahmednaeem/holosimulator/Home.java
+++ app/src/main/java/com/example/ahmednaeem/holosimulator/Home.java
@@ -12,9 +12,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class Home extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/ahmednaeem/holosimulator/HomeFragment.java
+++ app/src/main/java/com/example/ahmednaeem/holosimulator/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.ahmednaeem.holosimulator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,12 +8,31 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class HomeFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        ImageView publicSpeaking = (ImageView) rootView.findViewById(R.id.imageView2);
+
+        publicSpeaking.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ControllerActivity.class);
+                startActivity(intent);
+
+
+
+            }
+
+        });
+
+        return rootView;
     }
 }
